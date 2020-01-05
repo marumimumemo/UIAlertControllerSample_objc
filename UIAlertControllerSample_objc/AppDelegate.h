2eb3204c0e9f058751786f55d78ee55e817c3b19
marumimumemo <sagaminumber3@yahoo.co.jp>
2020-01-05 17:17:04 +0900

    Initial Commit

---
+++ UIAlertControllerSample_objc/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UIAlertControllerSample_objc
+//
+//  Created by 丸本聡 on 2020/01/05.
+//  Copyright © 2020 丸本聡. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ UIAlertControllerSample_objc/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  UIAlertControllerSample_objc
+//
+//  Created by 丸本聡 on 2020/01/05.
+//  Copyright © 2020 丸本聡. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
